24c4b9414fb9956e538876c22b7ea57f244d25f1
dharam-in <dharamdotin@gmail.com>
2024-07-23 17:15:56 +0530

    Graphql Todo Complete

--- frontend/src/components/TaskAdd.tsx
+++ frontend/src/components/TaskAdd.tsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import { useMutation } from '@apollo/client';
+import React, { useState } from 'react'
+import { CREATE_TODO } from '../queries/todoQuery';
 
 const TaskAdd = () => {
-  return (
-    <>
-      <form action="">
-          <div className='flex justify-between w-full'>
-            <input type="text" placeholder='Please Enter Your Task!' className='w-[90%] px-4'/>
-            <button className='bg-blue-500 px-4 py-2 text-white' type='submit'>Add</button>
-          </div>
-        </form>
-    </>
-  )
+    const [todotitle, setTodoTitle] = useState<String>();
+    const [createTodo, {data, error, loading}] = useMutation(CREATE_TODO);
+
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        createTodo({variables: {title: todotitle}});
+        if(error) alert(error.message);
+        setTodoTitle("")
+    }
+
+    return (
+        <>
+            <form action="" onSubmit={handleSubmit}>
+                <div className='flex justify-between w-full'>
+                    <input type="text" placeholder='Please Enter Your Task!' className='w-[90%] px-4' onChange={(e) => setTodoTitle(e.target.value)}/>
+                    <button className='bg-blue-500 px-4 py-2 text-white' type='submit'>Add</button>
+                </div>
+            </form>
+        </>
+    )
 }
 
 export default TaskAdd
--- frontend/src/components/TaskList.tsx
+++ frontend/src/components/TaskList.tsx
@@ -1,49 +1,32 @@
-import React from 'react'
+import { useQuery } from '@apollo/client';
+import { GET_TODOS } from '../queries/todoQuery';
+import React, { useState } from 'react';
 
 const TaskList = () => {
-
-    const TaskListData = [
-        {
-            title: "This is my first title",
-            completed: false
-        },
-        {
-            title: "This is my first title",
-            completed: true
-        },
-        {
-            title: "This is my first title",
-            completed: false
-        },
-        {
-            title: "This is my first title",
-            completed: false
-        },
-    ]
-
-  return (
-    <>
-      <div className='mt-4'>
-        <ul className='flex flex-col gap-5'>
-            {
-                TaskListData.map((data, index) => {
-                    return(
-                        <>
-                            <li className='flex justify-start w-full items-center gap-3'>
-                                <div className='w-[90%] flex gap-4 items-center'>
-                                    <input type="checkbox" className='w-4 h-4' id={`myInput${index}`}/>
-                                    <label className={`w-[90%] text-lg font-bold ${data.completed ? "line-through" : ""}`} htmlFor={`myInput${index}`}>{data.title}</label>
-                                </div>
-                                <button className='bg-blue-500 px-4 py-2 flex justify-end'>&#9876;</button>
-                            </li>
-                        </>
-                    )
-                })
-            }
-        </ul>
-      </div>
-    </>
-  )
+    const { data, loading, error } = useQuery(GET_TODOS);
+    const [todolist, setTodoList] = useState([]);
+    
+    return (
+        <>
+            <div className='mt-4'>
+                <ul className='flex flex-col gap-5'>
+                    {
+                        data && data.todos.map((data, index) => {
+                            return (
+                                <li className='flex justify-start w-full items-center gap-3' key={index}>
+                                    <div className='w-[90%] flex gap-4 items-center'>
+                                        <input type="checkbox" className='w-4 h-4' id={`myInput${index}`} />
+                                        <label className={`w-[90%] text-lg font-bold ${data.completed ? "line-through" : ""}`} htmlFor={`myInput${index}`}>{data.title}</label>
+                                    </div>
+                                    <button className='bg-blue-500 px-4 py-2 flex justify-end'>&#9876;</button>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+        </>
+    )
 }
 
 export default TaskList
